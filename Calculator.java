b2ae3147bbdada875c2fc7cc6f64f5cc1a63cf0c
Bhavadharani <dharanidharani59071@gmail.com>
2026-03-01 10:33:21 +0530

    Added manu-based Calculator with division validation

--- Calculator.java
+++ Calculator.java
@@ -2,14 +2,35 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-        System.out.println("Addition: " + (a + b));
-        System.out.println("Subtraction: " + (a - b));
-        System.out.println("Multiplication: " + (a * b));
-        System.out.println("Division: " + (a / b));
+        System.out.println("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.println("choose operation: ");
+        System.out.println("1.Addition" );
+        System.out.println("2.Subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+        int choice = sc.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("result: "+(a+b));
+                break;
+            case 2:
+                System.out.println("result: "+(a-b));
+                break;
+            case 3:
+                System.out.println("result: "+(a*b));
+                break;
+            case 4:
+                if(b!=0)
+                System.out.println("result: "+(a/b));
+                else
+                System.out.println("cannot divide by zero");
+                break;
+                default:
+                    System.out.println("invalid choice");
+                            }
         sc.close();
     }
 }

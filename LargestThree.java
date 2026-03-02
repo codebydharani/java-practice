1fa1d7317baea9f619b69776f1054baa0d2c9958
Bhavadharani <dharanidharani59071@gmail.com>
2026-03-02 17:27:44 +0530

    Added largest of three numbers program

---
+++ LargestThree.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class LargestThree {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 3 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a>=b && a>=c ){
+            System.out.print("A is Largest");
+        }
+        else if(b>=a && b>=c){
+            System.out.print("B is largest");
+        }
+        else{
+            System.out.print("C is largest");
+        }
+    }
+}

6f60e9fc08007d74819c68bdfabbd834e63b6fdd
Bhavadharani <dharanidharani59071@gmail.com>
2026-03-11 11:58:47 +0530

    feat: add sum of array elements program

---
+++ ArraySum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ArraySum{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of element:");
+        int n = sc.nextInt();
+        int[]arr=new int[n];
+        System.out.print("Enter elements:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum =0;
+        for(int i=0;i<n;i++){
+            sum = sum+arr[i];
+        }
+        System.out.println("Sum of element = "+sum);
+        sc.close();
+    }
+}

8ed1a4500c0af364f98614676368aea54ea0ae43
Bhavadharani <dharanidharani59071@gmail.com>
2026-03-26 22:15:33 +0530

    feat: find first and last occurrence using binary search

---
+++ FirstLastOccurrence.java
@@ -0,0 +1,40 @@
+public class FirstLastOccurrence {
+public static int firstOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1; // go left
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+    public static int lastOccurrence(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                left = mid + 1; // go right
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3, 4};
+        int target = 2;
+        System.out.println("First: " + firstOccurrence(arr, target));
+        System.out.println("Last: " + lastOccurrence(arr, target));
+    }
+}

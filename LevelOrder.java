87e37755a1d75c977fffc545605b30bffec021a8
Bhavadharani <dharanidharani59071@gmail.com>
2026-04-23 19:13:54 +0530

    feat:level order traversal

---
+++ LevelOrder.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class LevelOrder {
+    static class Node {
+        int data;
+        Node left, right;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static void levelOrder(Node root) {
+        if (root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node curr = q.poll();
+            System.out.print(curr.data + " ");
+            if (curr.left != null) q.add(curr.left);
+            if (curr.right != null) q.add(curr.right);
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);levelOrder(root);
+    }
+}
